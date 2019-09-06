64c5fc09302c93e2273e0e9d461f1c90c4f89ea1
ltosetti <l.tosetti@hotmail.it>
2019-09-06 11:34:01 +0200

    mofdify the short gameshit SA

--- spaceAce/gamesheetSA_short.js
+++ spaceAce/gamesheetSA_short.js
@@ -127,10 +127,209 @@ var data = {
                  "failStartTime":96.75, 
                 "failEndTime":97.75
             }, 
-            "finishStage":90,
+        },
+        {   // electric wall
+            "id":1,
+            "title": "run to take spaceship2",
+            "active":false,
+            "startTime":63.5,
+            "endTime":64.5,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"fire",
+            "wrongMove1":{               
+                "failStartTime":96.75, 
+                "failEndTime":97.75
+            },
+            "wrongMove2":{               
+                 "failStartTime":96.75, 
+                "failEndTime":97.75
+            }, 
+        },
+        {   // direnction
+            "id":1,
+            "title":"energy fire",
+            "active":false,
+            "startTime":67.5,
+            "endTime":68.5,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"fire",
+            "wrongMove1":{
+                "failStartTime":98.25, 
+                "failEndTime":99.5
+            },
+            "wrongMove2":{
+                "failStartTime":98.25, 
+                "failEndTime":99.5
+            }
+        },        
+        {   // blade 
+            "id":1,
+            "title": "energy jump",
+            "active":false,
+            "startTime":69.5,
+            "endTime":70.5,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"right",
+            "wrongMove1":{
+                "failStartTime":108.1, 
+                "failEndTime":110
+            },
+            "wrongMove2":{
+               "failStartTime":108.1, 
+                "failEndTime":110
+            }
+        },        
+        {   // platform mobile 
+            "id":1,
+            "title": "platform mobile",
+            "active":false,
+            "startTime":71.5,
+            "endTime":72,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"left",
+            "wrongMove1":{
+                "failStartTime":108.1, 
+                "failEndTime":110
+            },
+            "wrongMove2":{
+               "failStartTime":108.1, 
+                "failEndTime":110
+            }
+        },        
+        {   // platform mobile 
+            "id":1,
+            "title": "platform mobile",
+            "active":false,
+            "startTime":73.5,
+            "endTime":74.5,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"fire",
+            "wrongMove1":{
+                "failStartTime":104.25, 
+                "failEndTime":107.75
+            },
+            "wrongMove2":{
+                "failStartTime":98.25, 
+                "failEndTime":99.5
+            }
+        },
+        {   // platform mobile
+            "id":1,
+            "title": "platform mobile",
+            "active":false,
+            "startTime":76.2,
+            "endTime":77.2,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"left",
+            "wrongMove1":{
+                "failStartTime":108.1, 
+                "failEndTime":110
+            },
+            "wrongMove2":{
+               "failStartTime":108.1, 
+                "failEndTime":110
+            }
+        },
+        {   // platform mobile 
+            "id":1,
+            "title": "platform mobile",
+            "active":false,
+            "startTime":81,
+            "endTime":81.7,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"right",
+            "wrongMove1":{
+                "failStartTime":102.5, 
+                "failEndTime":104
+            },
+            "wrongMove2":{
+                 "failStartTime":102.5, 
+                "failEndTime":104
+            }
+        },
+        {   // platform mobile 
+            "id":1,
+            "title": "platform mobile",
+            "active":false,
+            "startTime":83,
+            "endTime":83.7,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"left",
+            "wrongMove1":{
+                "failStartTime":102.5, 
+                "failEndTime":104
+            },
+            "wrongMove2":{
+                 "failStartTime":102.5, 
+                "failEndTime":104
+            }
+        },
+        {   // platform mobile
+            "id":1,
+            "title": "platform mobile",
+            "active":false,
+            "startTime":85,
+            "endTime":85.7,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"left",
+            "wrongMove1":{
+                "failStartTime":102.5, 
+                "failEndTime":104
+            },
+            "wrongMove2":{
+                 "failStartTime":102.5, 
+                "failEndTime":104
+            }
+        },
+        {   // platform mobile 
+            "id":1,
+            "title": "platform mobile",
+            "active":false,
+            "startTime":87.7,
+            "endTime":88.5,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"left",
+            "wrongMove1":{
+                "failStartTime":102.5, 
+                "failEndTime":104
+            },
+            "wrongMove2":{
+                 "failStartTime":102.5, 
+                "failEndTime":104
+            }
+        },
+        {   // platform mobile
+            "id":1,
+            "jumpTo": 112.25,
+            "title": "platform mobile",
+            "active":false,
+            "startTime":91.2,
+            "endTime":92.2,
+            "complete": false,
+            "pressed": false,
+            "correctMove":"left",
+            "wrongMove1":{
+                "failStartTime":102.5, 
+                "failEndTime":104
+            },
+            "wrongMove2":{
+                 "failStartTime":102.5, 
+                "failEndTime":104
+            },
+            "finishStage":96,
             "gameComplete": true
-        }  
-        
+            
+        },        
     ],          
 };
 
